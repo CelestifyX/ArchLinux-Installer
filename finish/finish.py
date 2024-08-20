e8424a0cc5a452c179b08b0b869a17ea418e93bd
CELESTIFYX <celestifyx@gmail.com>
2024-08-20 18:34:29 +0300

    Remove all files

--- finish/finish.py
+++
@@ -1,8 +0,0 @@
-from functions.functions import *
-from colors.colors       import Colors
-
-def finish():
-    input(f'\n\n\n{Colors.green}SUCCESS{Colors.reset}: Installation completed. Press enter to reboot')
-    execute_command('reboot')
-
-    return True
--- status/status.py
+++
@@ -1,6 +0,0 @@
-from colors.colors import Colors
-
-class Status:
-    PROGRESS = '[.]'
-    OK       = f'[{Colors.light_green}\u2714{Colors.reset}]'
-    ERROR    = f'[{Colors.light_red}\u2718{Colors.reset}]'
